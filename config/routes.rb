c6481d301587ad022e45d69219fdf07484068ef4
ErickMtz <erick.e.mtz.g@gmail.com>
2021-01-29 12:06:11 -0600

    configure routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # For details on the DSL available within this file,
+  # see https://guides.rubyonrails.org/routing.html
+  resources :schools, only: [:create, :update, :destroy] do
+    resources :recipients, only: [:index, :create, :update, :destroy]
+    resources :orders, only: [:index, :create, :update] do
+      post :cancel
+      post :ship
+    end
+  end
 end
