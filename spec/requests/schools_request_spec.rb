6e0ddea5e3c025884205bd25961bf9bcbcc873d0
ErickMtz <erick.e.mtz.g@gmail.com>
2021-01-29 16:28:02 -0600

    Add docs for schools

--- spec/requests/schools_request_spec.rb
+++ spec/requests/schools_request_spec.rb
@@ -1,61 +1,124 @@
 require 'rails_helper'
+RSpec.shared_examples 'Unprocessable Entity Error' do
+  let(:request_body) { { } }
+  it 'returns an error' do
+    expect(subject).to have_http_status(:unprocessable_entity)
+  end
+end
+
+RSpec.shared_examples 'Not Found Error' do
+  let(:school_id) { 0 }
+  it 'returns an error' do
+    expect(subject).to have_http_status(:not_found)
+  end
+end
 
 RSpec.describe 'Schools', type: :request do
-  describe 'create school' do
-    subject do
-      post request_url, params: request_body.to_json, headers: request_headers
-      response
-    end
-    let(:request_url) { '/schools' }
-    let(:request_body) { { name: 'South Texas College' } }
-    let(:request_headers) { { CONTENT_TYPE: 'application/json' } }
-    let(:expected_body) { { 'name' => 'South Texas College' } }
+  path '/schools' do
+    post 'Creates a school' do
+      consumes 'application/json'
+      produces 'application/json'
+      parameter name: :school, in: :body, schema: {
+        type: :object,
+        properties: {
+          name: { type: :string },
+        },
+        required: ['name']
+      }
 
-    it 'creates a school and returns the record', :aggregate_failures do
-      expect { subject }.to change { School.count }.from(0).to(1)
-      expect(subject).to have_http_status(:ok)
-      expect(subject.parsed_body).to include(expected_body)
-    end
-  end
+      subject do
+        post request_url, params: request_body.to_json, headers: request_headers
+        response
+      end
+      let(:request_url) { '/schools' }
+      let(:request_body) { { name: 'South Texas College' } }
+      let(:request_headers) { { CONTENT_TYPE: 'application/json' } }
+      let(:expected_body) { { 'name' => 'South Texas College' } }
 
-  describe 'update school' do
-    subject do
-      put request_url, params: request_body.to_json, headers: request_headers
-      response
-    end
-    let(:request_url) { "/schools/#{school_id}" }
-    let(:request_body) { { name: 'University Of Michigan' } }
-    let(:request_headers) { { CONTENT_TYPE: 'application/json' } }
+      response '200', 'School Created' do
+        it 'creates a school and returns the record', :aggregate_failures do
+          expect { subject }.to change { School.count }.from(0).to(1)
+          expect(subject).to have_http_status(:ok)
+          expect(subject.parsed_body).to include(expected_body)
+        end
+      end
 
-    let!(:school) { FactoryBot.create :school, name: 'South Texas College' }
-    let(:school_id) { school.id }
-    let(:expected_body) { { 'name' => 'University Of Michigan' } }
-
-    it 'updates the school and returns the record', :aggregate_failures do
-      expect { subject }.to change { school.reload.name }.
-        from('South Texas College').to('University Of Michigan')
-      expect(subject).to have_http_status(:ok)
-      expect(subject.parsed_body).to include(expected_body)
+      response '422', 'Unprocessable Entity' do
+        it_behaves_like 'Unprocessable Entity Error'
+      end
     end
   end
 
-  describe 'delete school' do
-    subject do
-      delete request_url, params: request_body.to_json, headers: request_headers
-      response
+  path '/schools/{id}' do
+    put 'Updates a school' do
+      consumes 'application/json'
+      produces 'application/json'
+      parameter name: :id, in: :path
+      parameter name: :school, in: :body, schema: {
+        type: :object,
+        properties: {
+          name: { type: :string },
+        },
+        required: ['name']
+      }
+      subject do
+        put request_url, params: request_body.to_json, headers: request_headers
+        response
+      end
+      let(:request_url) { "/schools/#{school_id}" }
+      let(:request_body) { { name: 'University Of Michigan' } }
+      let(:request_headers) { { CONTENT_TYPE: 'application/json' } }
+
+      let!(:school) { FactoryBot.create :school, name: 'South Texas College' }
+      let(:school_id) { school.id }
+      let(:expected_body) { { 'name' => 'University Of Michigan' } }
+
+      response '200', 'School Updated' do
+        it 'updates the school and returns the record', :aggregate_failures do
+          expect { subject }.to change { school.reload.name }.
+            from('South Texas College').to('University Of Michigan')
+          expect(subject).to have_http_status(:ok)
+          expect(subject.parsed_body).to include(expected_body)
+        end
+      end
+
+      response '422', 'Unprocessable Entity' do
+        it_behaves_like 'Unprocessable Entity Error'
+      end
+      response '404', 'Not Found' do
+        it_behaves_like 'Not Found Error'
+      end
     end
-    let(:request_url) { "/schools/#{school_id}" }
-    let(:request_body) { { } }
-    let(:request_headers) { { CONTENT_TYPE: 'application/json' } }
+  end
 
-    let!(:school) { FactoryBot.create :school, name: 'South Texas College' }
-    let(:school_id) { school.id }
-    let(:expected_body) { {'name' => 'South Texas College' } }
+  path '/schools/{id}' do
+    delete 'Deletes a School' do
+      consumes 'application/json'
+      produces 'application/json'
+      parameter name: :id, in: :path
+      subject do
+        delete request_url, params: request_body.to_json, headers: request_headers
+        response
+      end
+      let(:request_url) { "/schools/#{school_id}" }
+      let(:request_body) { { } }
+      let(:request_headers) { { CONTENT_TYPE: 'application/json' } }
 
-    it 'deletes the school and returns the record', :aggregate_failures do
-      expect { subject }.to change { School.count }.from(1).to(0)
-      expect(subject).to have_http_status(:ok)
-      expect(subject.parsed_body).to include(expected_body)
+      let!(:school) { FactoryBot.create :school, name: 'South Texas College' }
+      let(:school_id) { school.id }
+      let(:expected_body) { {'name' => 'South Texas College' } }
+
+      response '200', 'School Deleted' do
+        it 'deletes the school and returns the record', :aggregate_failures do
+          expect { subject }.to change { School.count }.from(1).to(0)
+          expect(subject).to have_http_status(:ok)
+          expect(subject.parsed_body).to include(expected_body)
+        end
+      end
+
+      response '404', 'Not Found' do
+        it_behaves_like 'Not Found Error'
+      end
     end
   end
 end
