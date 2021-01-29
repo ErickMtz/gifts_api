1b2c4f4c47ca3fe9fe0ee81c25e0b9e4de62c5bd
ErickMtz <erick.e.mtz.g@gmail.com>
2021-01-29 12:04:41 -0600

    configure database cleaner

--- spec/support/database_cleaner.rb
+++ spec/support/database_cleaner.rb
@@ -2,19 +2,14 @@ DatabaseCleaner.allow_remote_database_url = true
 
 RSpec.configure do |config|
   config.before(:suite) do
-    DatabaseCleaner.clean_with :truncation, except: %w(ar_internal_metadata)
-  end
-  
-  config.before(:each) do
+    DatabaseCleaner.clean_with :truncation, except: %w[ar_internal_metadata statuses]
     DatabaseCleaner.strategy = :transaction
   end
 
-  config.before(:each) do
-    DatabaseCleaner.start
-  end
-
-  config.before(:each) do
-    DatabaseCleaner.clean
+  config.around(:each) do |example|
+    DatabaseCleaner.cleaning do
+      example.run
+    end
   end
 end
 
