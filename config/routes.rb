a96e19ac578e651b28a7fd33647e02e34911d914
ErickMtz <erick.e.mtz.g@gmail.com>
2021-01-30 18:40:59 -0600

    Add Doorkeeper

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
+  # create doorkeeper routes
+  use_doorkeeper
   # For details on the DSL available within this file,
   # see https://guides.rubyonrails.org/routing.html
   resources :schools, only: [:create, :update, :destroy] do
